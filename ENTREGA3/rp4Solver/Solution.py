3c14503244257b650ee0eb1a2a2acf82016d94e8
TomasCandeloM <tomascamo@unisabana.edu.co>
2024-05-13 16:05:05 -0500

    agrega codigo de solution y modelo

--- ENTREGA3/rp4Solver/Solution.py
+++ ENTREGA3/rp4Solver/Solution.py
@@ -13,22 +13,24 @@ except ImportError as e:
     from MARKOVIANAS import Markov_Model
     is_ipynb=True
 
-ford_matrix,Ford_last_stdv = Markov_Model.sendInfoToAnalize("Ford3",is_ipynb)
+ford_matrix,ford_last_stdv = Markov_Model.sendInfoToAnalize("Ford3",is_ipynb)
 NIO_matrix,NIO_last_stdv = Markov_Model.sendInfoToAnalize("NIO3",is_ipynb)
 
-print(ford_matrix)
 
 #S1-Día de la Inversión (0,1,2....)
 #S2-Dinero de la inversión en el día S1 (100,101,102...)
 #S3-Empresa en la que esta la inversión en el día S1 (Ford,Nio)
 
-empresas=["Ford","Nio"]
+# Bajar = 0 ; Subir = 1
 
 def Starting_State():    
     s1=1
-    s2=100
-    s3=empresas[0]
-    s=(s1,s2,s3)    
+    s2="Ford"
+    s3=100
+    s4 = 100
+    s5 = 1
+    s6 = 1
+    s=(s1,s2,s3,s4,s5,s6)
     return s
 
 #Si el dinero llega a 0, no puedo invertir
@@ -36,50 +38,80 @@ def Starting_State():
 #Si la acción bajo el día anterior, se cambia
 
 def Action_Set(s):
-    (s1,s2,s3)=s    
-    if s2<=0: AS=['myself']    
+    (s1,s2,s3,s4,s5,s6)=s  
+    if s1 >= 4: AS=['myself']    
     else: AS=['Cambio','Mantengo']       
     return AS
 
 def Event_Set(s,a):
-    (s1,s2,s3)=s
-    ES=['Sube','Baja']            
+    (s1,s2,s3,s4,s5,s6)=s
+    if s1 >= 4: ES=['se']
+    # Bajar = 0 ; Subir = 1
+    else: ES=[(0,0),(0,1),(1,0),(1,1)]            
     return ES
 
 def Transition_Equations(s,a,e):
-    (s1,s2,s3)=s    
+    (s1,s2,s3,s4,s5,s6)=s   
     sn1=s1+1
-    if a=='Cambio': sn2=s2-(s2*0.10)
-    if e=='Baja' and s3=='Ford': 
-        sn3='Nio'
-    elif e=='Baja' and s3=='Nio':
-        sn3='Ford'
+    if ((a == 'Mantengo' and s2 == 'Ford') or (a=='Cambio' and s2 == 'Nio')):
+        sn2 = "Ford"
+    elif ((a == 'Mantengo' and s2 == 'Nio') or (a=='Cambio' and s2 == 'Ford')):
+        sn2 = "Nio"
+    
+    if (e == (1,0) or e == (1,1)):
+        sn3 = s3 * (1 + ford_last_stdv)
+    elif (e == (0,0) or e == (0,1)):
+        sn3 = s3 * (1 - ford_last_stdv)
 
-    """Aqui voy porque estoy buscando como traer la desviación estándar"""
-    if s2>0 and e=='malo':
-        sn2=1
-    sn=(sn1,sn2,sn3)
+    if (e == (0,1) or e == (1,1)):
+        sn4 = s4 * (1 + NIO_last_stdv)
+    elif (e == (0,0) or e == (1,0)):
+        sn4 = s4 * (1 - NIO_last_stdv)
+    
+    if (e == (1,0) or e == (1,1)):
+        sn5 = 1
+    elif (e == (0,0) or e == (0,1)):
+        sn5 = 0
+
+    if (e == (0,1) or e == (1,1)):
+        sn6 = 1
+    elif (e == (0,0) or e == (1,0)):
+        sn6 = 0
+
+    sn=(sn1,sn2,sn3,sn4,sn5,sn6)
+        
     return sn
 
 def Constraints(s,a,sn,L):
-    (sn1,sn2)=sn
+    (sn1,sn2,sn3,sn4,sn5,sn6)=sn
     Ct=(sn1<=4)
     return Ct
 
 def Transition_Probabilities(s,a,e):
-    (s1,s2)=s
-    if a==0: p=1    
-    if a>0 and e=='malo': p=0.5**a
-    if a>0 and e=='bueno': p=1-(0.5**a)    
+    (s1,s2,s3,s4,s5,s6)=s
+    if e == (1,1):
+        p = ford_matrix[1][s5] * NIO_matrix[1][s6]
+    elif e == (1,0):    
+        p = ford_matrix[1][s5] * NIO_matrix[0][s6]
+    elif e == (0,1):
+        p = ford_matrix[0][s5] * NIO_matrix[1][s6]
+    elif e == (0,0):
+        p = ford_matrix[0][s5] * NIO_matrix[0][s6] 
     return p
     
 def Action_Contribution(s,a):
-    if a==0: ca=0
-    if a>0: ca=300+(100*a)
+    (s1,s2,s3,s4,s5,s6)=s
+    if a=="Mantengo": ca=0
+    if a=="Cambio" and s2 == 'Ford': ca=s3*0.01
+    if a=="Cambio" and s2 == 'Nio': ca=s4*0.01
     return ca
 
 def Event_Contribution(s,a,e):
-    ce=0 
+    (s1,s2,s3,s4,s5,s6)=s
+    if ((e == (1,0) or e == (1,1)) and s2 == 'Ford'): ce=s3*ford_last_stdv
+    if ((e == (0,0) or e == (0,1)) and s2 == 'Ford'): ce=-s3*ford_last_stdv
+    if ((e == (0,1) or e == (1,1)) and s2 == 'Nio'): ce=s4*NIO_last_stdv
+    if ((e == (0,0) or e == (1,0)) and s2 == 'Nio'): ce=-s4*NIO_last_stdv
     return ce
 
 def Quality_Function(m,p,ca,ce,V_sn):
@@ -87,11 +119,9 @@ def Quality_Function(m,p,ca,ce,V_sn):
     return Q_s_a
 
 def Optimal_Value_Function(Q_s_a):
-    V_s=min(Q_s_a)
+    V_s=max(Q_s_a)
     return V_s
 
 def Boundary_Condition(s):    
-    (s1,s2)=s
-    if (s1==4 and s2==1): V_s=1600
-    else: V_s=0
+    V_s=0
     return V_s
